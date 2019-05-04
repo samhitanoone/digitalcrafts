7ece9ddbabed1613c6e404d5998af8e5142698f5
Samhita Noone <samhita.noone@gmail.com>
2019-05-04 16:30:28 +0000

    test

---
+++ week1/js101/exercises/level3.js
@@ -0,0 +1,31 @@
+function lvl3exercise1 () {
+  // Create a "hello" and a "world", return the concatenation of the two
+var ex = "hello";
+var ex2 = "world";
+return ex + ex2;
+}
+
+function lvl3exercise2 () {
+  // Create a "hello" and a 12, return the concatenation of the two
+var hey = "hello";
+var num = 12;
+return hey + num;
+}
+
+function lvl3exercise3 () {
+  // Create a variable that equals 12 and convert it to a string with concatenation. Return it.
+var num = 12;
+return num + "";
+}
+
+function lvl3exercise4 () {
+  // Create a "hello world!" string. Return the length of the string
+var hey = "hello world!"
+return hey.length;
+}
+
+function lvl3exercise5 () {
+  // Create a "hello world!" string. Return the index of the word "world".
+var hey = "hello world!";
+return hey.indexOf("world");
+}
